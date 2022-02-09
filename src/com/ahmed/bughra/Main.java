3d27c1a475b7213ae62efff1eefcfc411c3e6df1
Ahmed Bughra <ahmedbughra@gmail.com>
2022-02-10 00:22:14 +0300

    initial commit

---
+++ src/com/ahmed/bughra/Main.java
@@ -0,0 +1,16 @@
+package com.ahmed.bughra;
+
+/**
+ * @Project: case1-UyCoder
+ * @Package: com.ahmed.bughra
+ * @Created: 2/10/2022 00:17
+ * @Author: Ahmed Bughra
+ * @Email: AhmedBughra@gmail.com
+ * @CreatedWith: IntelliJ IDEA
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is for homework");
+    }
+}
+
